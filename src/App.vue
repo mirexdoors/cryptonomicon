4d02428889cd1096aaf495e94b2c08552d141fb5
mirexdoors <mirex.doors@mail.ru>
2021-07-03 02:17:04 +0700

    add filter & pagination

--- src/App.vue
+++ src/App.vue
@@ -139,10 +139,66 @@
       </section>
 
       <template v-if="tickers.length">
+        <button
+          class="
+            my-4
+            mx-2
+            inline-flex
+            items-center
+            py-2
+            px-4
+            border border-transparent
+            shadow-sm
+            text-sm
+            leading-4
+            font-medium
+            rounded-full
+            text-white
+            bg-gray-600
+            hover:bg-gray-700
+            transition-colors
+            duration-300
+            focus:outline-none
+            focus:ring-2 focus:ring-offset-2 focus:ring-gray-500
+          "
+          v-if="page > 1"
+          @click="page = page - 1"
+        >
+          Назад
+        </button>
+        <button
+          class="
+            my-4
+            mx-2
+            inline-flex
+            items-center
+            py-2
+            px-4
+            border border-transparent
+            shadow-sm
+            text-sm
+            leading-4
+            font-medium
+            rounded-full
+            text-white
+            bg-gray-600
+            hover:bg-gray-700
+            transition-colors
+            duration-300
+            focus:outline-none
+            focus:ring-2 focus:ring-offset-2 focus:ring-gray-500
+          "
+          @click="page = page + 1"
+          v-if="hasNextPage"
+        >
+          Вперед
+        </button>
+
+        <div>Фильтр: <input v-model="filter" /></div>
         <hr class="w-full border-t border-gray-600 my-4" />
         <dl class="mt-5 grid grid-cols-1 gap-5 sm:grid-cols-3">
           <div
-            v-for="t in tickers"
+            v-for="t in filteredTickers()"
             :key="t.name"
             @click="select(t)"
             :class="{
@@ -254,17 +310,32 @@ export default {
     return {
       isPreloader: true,
       isSuggestionError: false,
+      filter: "",
+      page: 1,
       ticker: "",
       tickers: [],
       sel: null,
       graph: [],
       coins: [],
+      hasNextPage: false,
     };
   },
 
   async created() {
     const COINS_API =
       "https://min-api.cryptocompare.com/data/all/coinlist?summary=true";
+
+    const windowData = Object.fromEntries(
+      new URL(window.location).searchParams.entries()
+    );
+
+    if (windowData.filter) {
+      this.filter = windowData.filter;
+    }
+
+    if (windowData.page) {
+      this.page = windowData.page;
+    }
 
     const tickersData = localStorage.getItem("cryptonomicon-list");
 
@@ -301,7 +372,39 @@ export default {
         .slice(0, 4);
     },
   },
+
+  watch: {
+    filter() {
+      this.page = 1;
+      window.history.pushState(
+        null,
+        document.title,
+        `${window.location.pathname}?filter=${this.filter}&page=${this.page}`
+      );
+    },
+    page() {
+      window.history.pushState(
+        null,
+        document.title,
+        `${window.location.pathname}?filter=${this.filter}&page=${this.page}`
+      );
+    },
+  },
+
   methods: {
+    filteredTickers() {
+      const start = (this.page - 1) * 6;
+      const end = this.page * 6;
+
+      const filteredTickers = this.tickers.filter((ticker) =>
+        ticker.name.includes(this.filter)
+      );
+
+      this.hasNextPage = filteredTickers.length > end;
+
+      return filteredTickers.slice(start, end);
+    },
+
     add() {
       const currentTicker = {
         name: this.ticker,
@@ -309,7 +412,7 @@ export default {
       };
 
       this.tickers.push(currentTicker);
-
+      this.filter = "";
       localStorage.setItem("cryptonomicon-list", JSON.stringify(this.tickers));
       this.subscribeToUpdates(currentTicker.name);
       this.ticker = "";
